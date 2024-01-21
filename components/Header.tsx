c632cd34cd6d136cd52b032ee16c7943f17f39aa
vahide <vahidefarzane@gmail.com>
2024-01-21 13:21:50 +0330

    navbar and footer added

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import { CustomButton } from "@components";
+import  CustomButton  from "./CustomButton";
 
 const Hero = () => {
   const handleScroll = () => {
@@ -14,13 +14,13 @@ const Hero = () => {
   };
 
   return (
-    <div className="hero">
+    <div className="header">
       <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
+        <h1 className="header__title">
           Find, book, rent a car—quick and super easy!
         </h1>
 
-        <p className="hero__subtitle">
+        <p className="header__subtitle">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
@@ -31,12 +31,12 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className="hero__image-container">
-        <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+      <div className="header__image-container">
+        <div className="header__image">
+          <Image src="/header.png" alt="header" fill className="object-contain" />
         </div>
 
-        <div className="hero__image-overlay" />
+        <div className="header__image-overlay" />
       </div>
     </div>
   );
